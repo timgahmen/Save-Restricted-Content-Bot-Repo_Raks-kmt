0fcdc7b4bca9a41bef6c7d9cbc3962536ed1a99a
Raks-kmt <163742930+Raks-kmt@users.noreply.github.com>
2024-06-26 22:57:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from os import getenv
 API_ID = int(getenv("API_ID", "28526237"))
 API_HASH = getenv("API_HASH", "936db76a74f9a52cfb2cea8a62e4c20e")
 BOT_TOKEN = getenv("BOT_TOKEN", "7346915962:AAGt7U1tdKUkijAPKCJRgrB3ft_jwuEq-JU")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6486192717").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
