601163d9780e0b773b00727962c3ac5c18888f20
Raks-kmt <163742930+Raks-kmt@users.noreply.github.com>
2024-06-26 22:56:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+API_ID = int(getenv("API_ID", "28526237"))
+API_HASH = getenv("API_HASH", "936db76a74f9a52cfb2cea8a62e4c20e")
+BOT_TOKEN = getenv("BOT_TOKEN", "7346915962:AAGt7U1tdKUkijAPKCJRgrB3ft_jwuEq-JU")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "")
